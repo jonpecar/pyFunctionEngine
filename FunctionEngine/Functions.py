5f1085d491199d0b970772ec1120c5cfaf7327c3
Jonathan Pecar <jonathan.pecar@outlook.com>
2021-09-15 22:33:53 +1000

    Implemented generic function call to simplify new functions.

--- FunctionEngine/Functions.py
+++ FunctionEngine/Functions.py
@@ -7,65 +7,43 @@ def return_term(input) -> Term:
     else:
         return Term(input)
 
+def generic_function(function, args, formatting, bracketting_operations = [], operation = Operations.OTHER) -> Equation:
+    args_term = [return_term(arg) for arg in args]
+    args_val = [arg.value for arg in args_term]
+    result_val = function(*args)
+    
+    output_formats = Term._build_formatting(args_term, formatting, bracketting_operations)
+
+    result = Equation(result_val, output_formats, operation)
+    return result
+
 def sqrt(input : Term) -> Equation:
-    input = return_term(input)
-    new_val = input.value ** 0.5
-
-    output_formats = Term._build_formatting([input], r'\sqrt{{{}}}', [])
-
-    result = Equation(new_val, output_formats, Operations.POWER)
+    result = generic_function(pow, [input, 0.5], r'\sqrt{{{0}}}', operation=Operations.POWER)
     return result
 
 def sin(input) -> Equation:
-    input = return_term(input)
-
-    new_val = np.sin(input.value.to('radians'))
-    output_formats = Term._build_formatting([input], r'\sin{{({})}}', [])
-
-    result = Equation(new_val, output_formats, [])
+    result = generic_function(np.sin, [input], r'\sin{{({0})}}')
     return result
 
 def cos(input) -> Equation:
-    input = return_term(input)
-
-    new_val = np.cos(input.value.to('radians'))
-    output_formats = Term._build_formatting([input], r'\cos{{({})}}', [])
-
-    result = Equation(new_val, output_formats, [])
+    result = generic_function(np.cos, [input], r'\cos{{({0})}}')
     return result
 
 def tan(input) -> Equation:
-    input = return_term(input)
-
-    new_val = np.tan(input.value)
-    output_formats = Term._build_formatting([input], r'\tan{{({})}}', [])
-
-    result = Equation(new_val, output_formats, [])
+    result = generic_function(np.tan, [input], r'\tan{{({0})}}')
     return result
 
 def asin(input) -> Equation:
-    input = return_term(input)
-    new_val = np.arcsin(input.value)
-    output_formats = Term._build_formatting([input], r'\sin^{{-1}}{{({})}}', [])
-
-    result = Equation(new_val, output_formats, [])
+    result = generic_function(np.arcsin, [input], r'\sin^{{-1}}{{({0})}}')
     result.unit = 'radians'
     return result
 
 def acos(input) -> Equation:
-    input = return_term(input)
-    new_val = np.arccos(input.value)
-    output_formats = Term._build_formatting([input], r'\cos^{{-1}}{{({})}}', [])
-
-    result = Equation(new_val, output_formats, [])
+    result = generic_function(np.arccos, [input], r'\cos^{{-1}}{{({0})}}')
     result.unit = 'radians'
     return result
 
 def atan(input) -> Equation:
-    input = return_term(input)
-    new_val = np.arctan(input.value)
-    output_formats = Term._build_formatting([input], r'\tan^{{-1}}{{({})}}', [])
-
-    result = Equation(new_val, output_formats, [])
+    result = generic_function(np.arctan, [input], r'\tan^{{-1}}{{({0})}}')
     result.unit = 'radians'
     return result
